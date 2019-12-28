35ecd36b1b45bb67f26c175ff3d416e055f379b6
vickywane <vickywane@gmail.com>
2019-12-28 09:47:32 +0000

    ..

--- src/pages/auth/login/login.tsx
+++ src/pages/auth/login/login.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import { Card } from "react-bootstrap";
+import Flex from "styled-flex-component";
+
+const Body = styled.div`
+  padding: 1em;
+`;
+
+const Title = styled.h2``;
 
 const Login = () => {
   return (
-    <div>
-      {" "}
-      <p> Login here </p>{" "}
-    </div>
+    <Body>
+      <Flex justifyCenter>
+        <Card style={{ padding: "1em" }}>
+          <p> Login here </p>{" "}
+        </Card>
+      </Flex>
+    </Body>
   );
 };
 
