5b0e52f2b22024587c8d4b9aeb97b9d2f73fea75
vickywane <vickywane@gmail.com>
2019-12-30 00:41:06 +0000

    routes

--- src/pages/event/createEvent.tsx
+++ src/pages/event/createEvent.tsx
@@ -4,16 +4,30 @@ import Flex from "styled-flex-component";
 
 import { Header, Footer } from "../../components/";
 
-const Body = styled.div`0.5em`;
+const Input = styled.input`
+width : 25em
+height : 5vh
+border : 1px solid #000
+border-radius : 5px
+padding : 0.5em
+font-size : 1.1em
+paddingLeft : 15px
+`;
+
+const Body = styled.div`
+  padding: 1em;
+`;
 const CreateEvent = () => {
   return (
     <div>
       <Header />
 
       <Body>
-        <p> create event </p>
+        <Flex column>
+          <label> Event Name </label>
+          <Input placeholder="Event Name" />
+        </Flex>
       </Body>
-
       <Footer />
     </div>
   );
