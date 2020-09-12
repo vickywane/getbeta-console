9855457064f97a66b53c2ed72a69f2745e4957cb
Vickywane <vickywane@gmail.com>
2020-09-13 00:13:21 +0100

    Upgrade account

--- src/state/vendor.store.js
+++ src/state/vendor.store.js
@@ -1,11 +1,25 @@
 import { action, observable, decorate } from 'mobx'
+import Axios from 'axios'
+
+const VendorEndpoint = process.emv.REACT_APP_ENDPOINT
+const token = localStorage.getItem('token')
 
 class VendorStore {
-  createVendor = () => {}
+  upgradeToVendor = (Email, Bio, MobileNo, FullName) => {
+    Axios.post(
+      `${VendorEndpoint}/vendors`,
+      { data: {} },
+      {
+        headers: {
+          'x-auth-token': token
+        }
+      }
+    )
+  }
 }
 
 const DecoratedVendorStore = decorate(VendorStore, {
-  createVendor: action
+  upgradeToVendor: action
 })
 
 const store = new DecoratedVendorStore()
